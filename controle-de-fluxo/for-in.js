6174d4d3ada40a1d61b5c13d5f6c40dce612b786
Willian_Wallace <125695509+WillianWallaceS@users.noreply.github.com>
2025-01-03 15:18:18 -0300

    Update for-in.js

--- controle-de-fluxo/for-in.js
+++ controle-de-fluxo/for-in.js
@@ -6,6 +6,8 @@ const users = {
 
 }
 
+//FOR IN Para objetos e FOR OF para arrays 
+
 for(const prop in users ){//Serve para localizar propriedades no código
     console.log(prop) //Prop é uma forma reduzida de dizer propriedades. 
 } 
@@ -18,3 +20,33 @@ for(const key in users){
         users[key] = 'novo valor'
     }
 }
+
+for(const key in users){
+    if (key === 'name' || key === 'email') { //Pegando as propriedades e fazendo modificações 
+        users[key] = users[key].toLowerCase()//Serve para diminuir as letras
+
+    }
+    
+}
+
+for(const key in users){
+    if (key === 'name'){
+        const names = users[key].split(' ') //Dividiu o Nome e Sobrenome transformando em uma Array e limpando os nomes para ficar mais legivel
+        users[key] = '' //continuação do split
+        for (const name of names){
+            const normalizedNames = name.toLowerCase()
+             const [primeiraLetra, ...restoDoNome] = normalizedNames //Separou cada letra do nome, pegando a primeira letra e o resto do nome 
+        
+             
+             users[key] = users[key] + ' '+ primeiraLetra.toUpperCase() + restoDoNome.join('')//colocou a primeira letra maiuscula e juntou o resto do nome e colocou no final 
+             users[key] = users[key].trim() // Para tirar espaços indevidos tanto do começo quanto do fim
+             
+        }
+      
+
+    }
+    if (key === 'email'){
+        users[key] = users[key].toLowerCase()
+    }
+}
+console.log(users)
