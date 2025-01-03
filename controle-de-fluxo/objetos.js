f29891d639ba144782d7f49ced457fb39457a85f
Willian_Wallace <125695509+WillianWallaceS@users.noreply.github.com>
2025-01-03 14:38:12 -0300

    Objetos V2
    Trabalhei com Objeto onde descreve uma espécie de animal e como pode ser representado de forma mais organizada e precisa

--- controle-de-fluxo/objetos.js
+++ controle-de-fluxo/objetos.js
@@ -1,2 +1,13 @@
-const animal = {}
+const animal = {
+    especie: 'Canis Lupus',
+    habitat: 'Florestas',
+    expectativaDeVida: 20,
+    terrestreAquaticoAereo: 'Terrestre'
+} //Com o objeto conseguimos organizar os dados mais rápido
+
+
 console.log(animal)
+console.log(animal.habitat) //serve para pegar a informação que quer
+
+animal.populacao = 1_000_000_000_000_000_000_000 
+console.log(animal.populacao)
