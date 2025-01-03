987c49cd69f4761b0acead801e4027be7397941c
Willian_Wallace <125695509+WillianWallaceS@users.noreply.github.com>
2025-01-03 14:15:12 -0300

    For Of
    Construções de código onde aprende sobre o uso de controle de fluxo com FOR e OF

---
+++ controle-de-fluxo/for-of.js
@@ -0,0 +1,25 @@
+//Código que conta de 0 a 100 
+
+    for (let contador = 0; contador < 100; contador++ ){
+      console.log(contador)
+    }
+//------------------------------------------------------------------
+
+//Mostra a lista de Números 
+//Código imperativo FOR
+const listaDeNumeros = [33, 131, 155, 555, 222, -21, -56]
+
+   for (let contador = 0; contador < listaDeNumeros.length; contador +=1 ){
+       console.log(listaDeNumeros[contador])
+  }
+
+//Código declarativo  FOR OF
+//Fez a mesma coisa só que mais simplificado
+for (const numero of listaDeNumeros){
+    console.log(numero)
+}
+ const usuarios = ['Antonia', 'Carolina']
+
+for (const usuario of usuarios ){
+    console.log(usuario)
+}
