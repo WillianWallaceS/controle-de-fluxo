1d62b065b46c20717bf9687cdf871a69b1d242e3
WillianWallaceGomes <125695509+WillianWallace2023@users.noreply.github.com>
2025-01-04 19:21:24 -0300

    HTTP
    Estudo de Http, colocando um HTTP no navegador

---
+++ controle-de-fluxo/http.js
@@ -0,0 +1,28 @@
+//requisições e servidores
+//HTTP que vai permitir criar um servidor que fornece informações como um cliente que pode consumi-las 
+//Requisição é quando eu busco um recurso remoto e quero resgatar alguma informação pode ter respostas como : arquivos, objetos js, xml. 
+
+const http = require('node:http')
+
+const server = http.createServer((request, response) => { 
+    const {method, statusCode, url} = request
+    const esportes = ['Volei', 'Basquete', 'Tennis']
+
+    if(url === '/'){
+       response.write('<div> <h1>Hello2from node</h1> <p>http server</p></div>')
+        response.end()
+    }
+
+    if(url === '/esportes'){
+        response.write(JSON.stringify(esportes))
+        response.end()
+    }
+
+})
+
+server.listen(3000, 'localhost', () =>{
+    console.log('Server running on http://localhost:3000') //Roda servidor pelo chrome
+})
+   
+
+
